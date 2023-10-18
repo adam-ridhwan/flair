5aa37e57ad34d5ca6faa952465f02587e61ce56f
Adam Ridhwan <adamridhwan.1@gmail.com>
2023-10-18 14:48:11 -0400

    feature: stripe payment

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,6 +7,4 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
-  token:
-    "skU3cdAyHH4yhUcYDEh4wquoVKjNpXbXmlnloVjZ3BlZP4eNZgvlSG14gqh2hJzXRY7L3k0ZPB9Fc2aKWoI8yHzMnB4ozQG90OyxSXLMxFl7FJzeBziqdmIkn30rhcsFJwbeV8sf9D0etSB67W7EpFYa93SUe6RIVaQBeF0KZymPRow4DHvA",
 })
